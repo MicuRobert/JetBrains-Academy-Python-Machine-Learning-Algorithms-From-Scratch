520423e0d2b576297cf04cf5a62c4989c720e62e
Micu Robert <micurobert99@gmail.com>
2022-12-29 00:50:38 +0200

    Stage 3/4

--- Linear Regression from Scratch.py
+++ Linear Regression from Scratch.py
@@ -1,14 +1,15 @@
 # write your code here
+import math
+
 import numpy
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-    {   'x': [4.0, 4.5, 5, 5.5, 6.0, 6.5, 7.0],
-        'w': [1, -3, 2, 5, 0, 3, 6],
-        'z': [11, 15, 12, 9, 18, 13, 16],
-        'y': [33, 42, 45, 51, 53, 61, 62]
-    })
+df = pd.DataFrame({
+        'Capacity': [0.9, 0.5, 1.75, 2.0, 1.4, 1.5, 3.0, 1.1, 2.6, 1.9],
+        'Age': [11, 11, 9, 8, 7, 7, 6, 5, 5, 4],
+        'Cost/ton': [21.95, 27.18, 16.9, 15.37, 16.03, 18.15, 14.22, 18.72, 15.4, 14.69],
+})
 
 
 class CustomLinearRegression:
@@ -21,10 +22,10 @@ class CustomLinearRegression:
 
     def fit(self, X, y):
         if self.fit_intercept:
+            X.insert(loc=0, column='ones', value=[1 for _ in range(1, X.shape[0] + 1)])
             model_coef = np.dot(np.dot(np.linalg.inv(np.dot(numpy.transpose(X), X)), numpy.transpose(X)), y)
             self.intercept = model_coef[0]
             self.coefficient = model_coef[1:]
-            return{ 'Intercept' : self.intercept, 'Coefficient' : self.coefficient }
         else:
             model_coef = np.dot(np.dot(np.linalg.inv(np.dot(numpy.transpose(X), X)), numpy.transpose(X)), y)
             self.intercept = 0
@@ -32,10 +33,32 @@ class CustomLinearRegression:
             pass
 
     def predict(self, X):
-        return np.dot(X, self.coefficient)
+        if self.fit_intercept:
+            X.insert(loc=0, column='ones', value=[1 for _ in range(1, X.shape[0]+1)])
+            return np.dot(X, [self.intercept, *self.coefficient])
+        else:
+            return np.dot(X, self.coefficient)
+
+    def r2_score(self, y, yhat):
+        yhat_mean = np.mean(yhat)
+        y_pairs = zip(y, yhat)
+        return 1 - sum([(ys-yhats) ** 2 for ys, yhats in y_pairs]) / sum([(ys - yhat_mean) ** 2 for ys in y])
+
+    def rmse(self, y, yhat):
+        y_pairs = zip(y, yhat)
+        mse = 1/y.shape[0] * sum([(ys - yhats) ** 2 for ys, yhats in y_pairs])
+        return math.sqrt(mse)
 
 
-reg = CustomLinearRegression(fit_intercept=False)
-reg.fit(df[['x', 'w', 'z']], df['y'])
-y_pred = reg.predict(df[['x', 'w', 'z']])
-print(y_pred)
+reg = CustomLinearRegression(fit_intercept=True)
+reg.fit(df[['Capacity', 'Age']], df['Cost/ton'])
+y_pred = reg.predict(df[['Capacity', 'Age']])
+r2 = reg.r2_score(df['Cost/ton'], y_pred)
+rmse = reg.rmse(df['Cost/ton'], y_pred)
+res = {
+        'Intercept': reg.intercept,
+        'Coefficient': reg.coefficient,
+        'R2': r2,
+        'RMSE': rmse
+    }
+print(res)
