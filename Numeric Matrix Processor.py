9de3a4aed70ef1322818a030657dca493f30e4d8
Micu Robert <micurobert99@gmail.com>
2022-12-30 00:01:36 +0200

    Stage 5/6

--- Numeric Matrix Processor.py
+++ Numeric Matrix Processor.py
@@ -3,39 +3,71 @@
 class MatrixProcessor:
     def __init__(self):
         self.print_matrix = self.print_matrix
-        pass
+        self.matrix_A = ...
+        self.matrix_B = ...
+        self.size_A = ...
+        self.size_B = ...
 
-    def sum_two_matrix(self, m1, m2, s1, s2):
-        if s1 == s2:
-            sum = [[m1[i][j] + m2[i][j] for j in range(s1[1])] for i in range(s1[0])]
+    def insert_A(self):
+        size_A = tuple([int(x) for x in input('Enter size of first matrix').split(' ')])
+        print('Enter first matrix:')
+        matrix_A = [list(map(lambda x: int(x) if x.isdigit() else float(x), input().split(' '))) for row in
+                    range(size_A[0])]
+        self.size_A = size_A
+        self.matrix_A = matrix_A
+
+    def insert_B(self):
+        size_B = tuple([int(x) for x in input('Enter size of second matrix').split(' ')])
+        print('Enter second matrix:')
+        matrix_B = [list(map(lambda x: int(x) if x.isdigit() else float(x), input().split(' '))) for row in
+                    range(size_B[0])]
+        self.size_B = size_B
+        self.matrix_B = matrix_B
+
+    def sum_two_matrix(self):
+        if self.size_A == self.size_B:
+            sum = [[self.matrix_A[i][j] + self.matrix_B[i][j] for j in range(self.size_A[1])] for i in range(self.size_A[0])]
             self.print_matrix(sum)
         else:
             print('The operation cannot be performed.')
 
-    def multi_by_num(self, m1, c, s):
-        multi_by_number = [list(map(lambda x: x * c, m1[i])) for i in range(s[0])]
+    def multi_by_num(self, c):
+        multi_by_number = [list(map(lambda x: x * c, self.matrix_A[i])) for i in range(self.size_A[0])]
         self.print_matrix(multi_by_number)
 
-    def multi_two_matrix(self, m1, m2, s1, s2):
-        if s1[1] == s2[0]:
-            multi_matrix = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*m2)] for X_row in m1]
+    def multi_two_matrix(self):
+        if self.size_A[1] == self.size_B[0]:
+            multi_matrix = [[sum(a*b for a,b in zip(X_row,Y_col)) for Y_col in zip(*self.matrix_B)] for X_row in self.matrix_A]
             self.print_matrix(multi_matrix)
         else:
             print('The operation cannot be performed.')
 
-    def transpose_square_matrix(self, m, s, choice):
+    def transpose_square_matrix(self, choice):
         if choice == 1:
-            self.print_matrix(map(list, zip(*m)))
-            pass
+            self.print_matrix(map(list, zip(*self.matrix_A)))
         elif choice == 2:
-            self.print_matrix(list(map(list, zip(*m[::-1])))[::-1])
-            pass
+            self.print_matrix(list(map(list, zip(*self.matrix_A[::-1])))[::-1])
         elif choice == 3:
-            self.print_matrix([x[::-1] for x in m])
-            pass
+            self.print_matrix([x[::-1] for x in self.matrix_A])
         elif choice == 4:
-            self.print_matrix(m[::-1])
-            pass
+            self.print_matrix(self.matrix_A[::-1])
+
+    def calc_det(self, m, s):
+        if s == (1,1):
+            return m[0][0]
+        if s == (2, 2):
+            return m[0][0] * m[1][1] - m[0][1] * m[1][0]
+
+        det = 0
+        for index, x in enumerate(m[0]):
+            sub_matrix = [[*arr[0:index],*arr[index+1:]] for arr in m[1:]]
+            sub_matrix_shape = (s[0]-1, s[1]-1)
+            subdet = self.calc_det(sub_matrix, sub_matrix_shape)
+            if index % 2 == 0:
+                det += x * subdet
+            else:
+                det -= x * subdet
+        return det
 
     def print_matrix(self, m):
         print('The result is:')
@@ -48,47 +80,36 @@ processor = MatrixProcessor()
 
 user_choice = 1
 while user_choice != 0:
-    print('''
-1. Add matrices
+    print('''1. Add matrices
 2. Multiply matrix by a constant
 3. Multiply matrices
 4. Transpose matrix
-0. Exit
-        ''')
+5. Calculate a determinant
+0. Exit''')
     user_choice = int(input('Your choice:'))
     if user_choice == 1:
-        size_A = tuple([int(x) for x in input('Enter size of first matrix').split(' ')])
-        print('Enter first matrix:')
-        matrix_A = [list(map(float, input().split(' '))) for row in range(size_A[0])]
-        size_B = tuple([int(x) for x in input('Enter size of second matrix').split(' ')])
-        print('Enter second matrix:')
-        matrix_B = [list(map(float, input().split(' '))) for row in range(size_B[0])]
-        processor.sum_two_matrix(matrix_A, matrix_B, size_A, size_B)
+        processor.insert_A()
+        processor.insert_B()
+        processor.sum_two_matrix()
     elif user_choice == 2:
-        size_A = tuple([int(x) for x in input('Enter size of matrix').split(' ')])
-        print('Enter matrix:')
-        matrix_A = [list(map(float, input().split(' '))) for row in range(size_A[0])]
+        processor.insert_A()
         constant = int(input('Enter constant:'))
-        processor.multi_by_num(matrix_A, constant, size_A)
+        processor.multi_by_num(constant)
     elif user_choice == 3:
-        size_A = tuple([int(x) for x in input('Enter size of first matrix').split(' ')])
-        print('Enter first matrix:')
-        matrix_A = [list(map(float, input().split(' '))) for row in range(size_A[0])]
-        size_B = tuple([int(x) for x in input('Enter size of second matrix').split(' ')])
-        print('Enter second matrix:')
-        matrix_B = [list(map(float, input().split(' '))) for row in range(size_B[0])]
-        processor.multi_two_matrix(matrix_A, matrix_B, size_A, size_B)
+        processor.insert_A()
+        processor.insert_B()
+        processor.multi_two_matrix()
     elif user_choice == 4:
-        print('''
-            1. Main diagonal
-            2. Side diagonal
-            3. Vertical line
-            4. Horizontal line
-            ''')
+        print('''1. Main diagonal
+2. Side diagonal
+3. Vertical line
+4. Horizontal line''')
         user_choice_transpose = int(input('Your choice:'))
-        size_A = tuple([int(x) for x in input('Enter size of first matrix').split(' ')])
-        print('Enter first matrix:')
-        matrix_A = [list(map(float, input().split(' '))) for row in range(size_A[0])]
-        processor.transpose_square_matrix(matrix_A, size_A, user_choice_transpose)
+        processor.insert_A()
+        processor.transpose_square_matrix(user_choice_transpose)
+    elif user_choice == 5:
+        processor.insert_A()
+        det = processor.calc_det(processor.matrix_A, processor.size_A)
+        print(f'The result is:\n{det}')
     elif user_choice == 0:
         exit()
