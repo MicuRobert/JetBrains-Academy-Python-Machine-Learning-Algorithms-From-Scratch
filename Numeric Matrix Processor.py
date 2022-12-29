28046f377794ac0dc4a3eeb6902ff88c855d0ea5
Micu Robert <micurobert99@gmail.com>
2022-12-29 13:22:17 +0200

    Stage 2/6

--- Numeric Matrix Processor.py
+++ Numeric Matrix Processor.py
@@ -2,13 +2,32 @@
 
 size_A = tuple([int(x) for x in input().split(' ')])
 matrix_A = [list(map(int, input().split(' '))) for row in range(size_A[0])]
-size_B = tuple([int(x) for x in input().split(' ')])
-matrix_B = [list(map(int, input().split(' '))) for row in range(size_B[0])]
+constant = int(input())
+# size_B = tuple([int(x) for x in input().split(' ')])
+# matrix_B = [list(map(int, input().split(' '))) for row in range(size_B[0])]
 
-if size_A == size_B:
-    sum = [[matrix_A[i][j] + matrix_B[i][j] for j in range(size_A[1])] for i in range(size_A[0])]
-    for row in sum:
-        row = [str(x) for x in row]
-        print(' '.join(row))
-else:
-    print('ERROR')
+
+class MatrixProcessor:
+    def __init__(self):
+        self.print_matrix = self.print_matrix
+        pass
+
+    def sum_two_matrix(self, m1, m2, s1, s2):
+        if s1 == s2:
+            sum = [[m1[i][j] + m2[i][j] for j in range(s1[1])] for i in range(s1[0])]
+            self.print_matrix(sum)
+        else:
+            print('ERROR')
+
+    def multi_by_num(self, m1, c, s):
+        multi_by_number = [list(map(lambda x: x * c, m1[i])) for i in range(s[0])]
+        self.print_matrix(multi_by_number)
+
+    def print_matrix(self, m):
+        for row in m:
+            row = [str(x) for x in row]
+            print(' '.join(row))
+
+
+processor = MatrixProcessor()
+processor.multi_by_num(matrix_A, constant, size_A)
