ecb223523f0a41f1251859377ba74d4f57354ce5
Micu Robert <micurobert99@gmail.com>
2023-01-03 00:27:29 +0200

    Stage 2/5

--- Salary Prediction.py
+++ Salary Prediction.py
@@ -19,20 +19,29 @@ if 'data.csv' not in os.listdir('../Data'):
 # read data
 data = pd.read_csv('../Data/data.csv')
 
-# X a dataframe with a predictor rating and y a series with a target salary
-X = (data['rating'].to_numpy()).reshape(-1,1)
-y = data['salary'].to_numpy()
+# dict for power and results predictor
+pw_res = {
+    2 : [],
+    3 : [],
+    4 : []
+}
 
-# split into training and test parts
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+for power in pw_res.keys():
+    # X a dataframe with a predictor rating and y a series with a target salary
+    X, y = data[['rating']] ** power, data['salary']
 
-# fit linear regression model
-model = LinearRegression()
-model.fit(X_train, y_train)
+    # split into training and test parts
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
-# predict model on test data and calc MAPE
-y_pred = model.predict(X_test)
-mape = mean_absolute_percentage_error(y_test, y_pred)
+    # fit linear regression model
+    model = LinearRegression()
+    model.fit(X_train, y_train)
 
-# print results
-print(round(model.intercept_,5), round(model.coef_[0],5), round(mape,5))
+    # predict model on test data and calc MAPE
+    y_pred = model.predict(X_test)
+    mape = mean_absolute_percentage_error(y_test, y_pred)
+
+    pw_res[power] = [round(model.intercept_,5), round(model.coef_[0],5), round(mape,5)]
+
+# print best results
+print(pw_res[min(pw_res, key = lambda k: pw_res[k][2])][2])
