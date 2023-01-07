54437a016f76a0f25ac4517426a3b965e328f500
Micu Robert <micurobert99@gmail.com>
2023-01-08 01:07:52 +0200

    Stage 4/5

--- Salary Prediction.py
+++ Salary Prediction.py
@@ -1,10 +1,12 @@
 import os
-import requests
 
+import numpy as np
+import requests
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_percentage_error
+from itertools import combinations_with_replacement
 
 # checking ../Data directory presence
 if not os.path.exists('../Data'):
@@ -19,19 +21,34 @@ if 'data.csv' not in os.listdir('../Data'):
 # read data
 data = pd.read_csv('../Data/data.csv')
 
+# data correlation
+data_corr = data.corr()
+
+# correlations > 0.2
+corr = list(data_corr.nlargest(4, 'salary').index)[1:]
+
 # X a dataframe with a predictor rating and y a series with a target salary
-X, y = data.loc[:, ['rating', 'draft_round', 'age', 'experience', 'bmi']] , data['salary']
+X, y = data.loc[:, ['rating', 'draft_round', 'age', 'experience', 'bmi']], data['salary']
 
 # split into training and test parts
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
 
-# fit linear regression model
+# linear regression model
 model = LinearRegression()
-model.fit(X_train, y_train)
 
-# predict model on test data and calc MAPE
-y_pred = model.predict(X_test)
-mape = mean_absolute_percentage_error(y_test, y_pred)
+# comb to drop
+comb_to_drop = [np.unique(np.array(comb)) for comb in combinations_with_replacement(corr, 2)]
+
+mape_res = []
+model.fit(X_train, y_train)
+for drop_col in comb_to_drop:
+    # fit model with the new subset
+    model.fit(X_train.drop(drop_col, axis=1), y_train)
+
+    # predict model on test data and calc MAPE
+    y_pred = model.predict(X_test.drop(drop_col, axis=1))
+    mape = round(mean_absolute_percentage_error(y_test, y_pred), 5)
+    mape_res.append(mape)
 
 # print best results
-print(*[coef for coef in model.coef_], sep=', ')
+print(min(mape_res))
