d9c51bbe9688032dc8b5cdb1caf10fda4fd9311e
Micu Robert <micurobert99@gmail.com>
2023-03-09 01:42:22 +0200

    Stage 7/7

--- Neural Network from Scratch.py
+++ Neural Network from Scratch.py
@@ -41,6 +41,12 @@ def accuracy(model, X, y):
     y_true = np.argmax(y, axis=1)
     return np.mean(y_pred == y_true)
 
+def epoch_train(estimator, alpha, X, y, batch_size, X_t, y_t):
+    n = X.shape[0]
+    for i in range(0, n, batch_size):
+        model.backprop(X[i:i + batch_size], y[i:i + batch_size], alpha)
+    return accuracy(estimator, X_t, y_t)
+
 class OneLayerNeural:
     def __init__(self, n_features, n_classes):
         self.weights = xavier(n_features, n_classes)
@@ -145,6 +151,5 @@ if __name__ == '__main__':
     X_test = scale(X_test)
 
     model = TwoLayerNeural(np.shape(X_train)[1], 10)
-    model.backprop(X_train[:2], y_train[:2], alpha=0.1)
-    y_pred = model.forward(X_train[:2])
-    print(mse(y_pred, y_train[:2]).flatten().tolist())
+    answ = [epoch_train(model, 0.5, X_train, y_train, 100, X_test, y_test) for _ in range(20)]
+    print(answ)
