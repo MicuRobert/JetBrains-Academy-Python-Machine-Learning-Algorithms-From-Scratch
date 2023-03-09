e7115e0dcd614a7d374e261d36c44a44f095ba94
Micu Robert <micurobert99@gmail.com>
2023-03-09 09:51:01 +0200

    Stage 3/5

--- Classification of Handwritten Digits.py
+++ Classification of Handwritten Digits.py
@@ -1,21 +1,38 @@
-import numpy as np
-import tensorflow as tf
 from sklearn.model_selection import train_test_split
-import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from tensorflow.keras.datasets import mnist
+import numpy as np
 
-(x_train, y_train), _ = tf.keras.datasets.mnist.load_data()
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(X_train.shape[0], -1)
+X_test = X_test.reshape(X_test.shape[0], -1)
+x = np.concatenate((X_train, X_test))[:6000]
+y = np.concatenate((y_train, y_test))[:6000]
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=40)
 
-x_train = x_train.reshape(x_train.shape[0], -1)
+models = {
+    'K-nearest Neighbors': KNeighborsClassifier(),
+    'Decision Tree': DecisionTreeClassifier(random_state=40),
+    'Logistic Regression': LogisticRegression(solver="liblinear", random_state=40),
+    'Random Forest': RandomForestClassifier(random_state=40)
+}
 
-x_train = x_train[:6000]
-y_train = y_train[:6000]
+# Define the fit_predict_eval function
+def fit_predict_eval(model, features_train, features_test, target_train, target_test):
+    model.fit(features_train, target_train)
+    y_pred = model.predict(features_test)
+    score = accuracy_score(target_test, y_pred)
+    print(f'Model: {model.__class__.__name__}\nAccuracy: {score:.4f}\n')
 
-x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.3, random_state=40)
+# Train and evaluate the models
+for name, model in models.items():
+    fit_predict_eval(model, X_train, X_test, y_train, y_test)
 
-print("x_train shape:", x_train.shape)
-print("x_test shape:", x_test.shape)
-print("y_train shape:", y_train.shape)
-print("y_test shape:", y_test.shape)
-
-train_class_proportions = pd.Series(y_train).value_counts(normalize=True)
-print("Proportion of samples per class in train set:\n", train_class_proportions)
+# Determine which model performed the best
+best_model = max(models, key=lambda x: accuracy_score(y_test, models[x].predict(X_test)))
+best_score = accuracy_score(y_test, models[best_model].predict(X_test))
+print(f"The answer to the question: {models[best_model].__class__.__name__} - {best_score:.3f}")
