20161663a179280fb56db65a3d75ed83f8ccd658
Micu Robert <micurobert99@gmail.com>
2023-03-13 10:02:23 +0200

    Stage 4/5

--- Classification of Handwritten Digits.py
+++ Classification of Handwritten Digits.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from tensorflow.keras.datasets import mnist
+from sklearn.preprocessing import Normalizer
 import numpy as np
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -14,6 +15,11 @@ x = np.concatenate((X_train, X_test))[:6000]
 y = np.concatenate((y_train, y_test))[:6000]
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=40)
 
+normalizer = Normalizer()
+
+X_train_norm = normalizer.fit_transform(X_train)
+X_test_norm = normalizer.transform(X_test)
+
 models = {
     'K-nearest Neighbors': KNeighborsClassifier(),
     'Decision Tree': DecisionTreeClassifier(random_state=40),
@@ -21,18 +27,17 @@ models = {
     'Random Forest': RandomForestClassifier(random_state=40)
 }
 
-# Define the fit_predict_eval function
+
 def fit_predict_eval(model, features_train, features_test, target_train, target_test):
     model.fit(features_train, target_train)
     y_pred = model.predict(features_test)
     score = accuracy_score(target_test, y_pred)
-    print(f'Model: {model.__class__.__name__}\nAccuracy: {score:.4f}\n')
+    print(f'Model: {model.__class__.__name__}\nAccuracy: {score:.3f}\n')
+    return score
 
-# Train and evaluate the models
-for name, model in models.items():
-    fit_predict_eval(model, X_train, X_test, y_train, y_test)
 
-# Determine which model performed the best
-best_model = max(models, key=lambda x: accuracy_score(y_test, models[x].predict(X_test)))
-best_score = accuracy_score(y_test, models[best_model].predict(X_test))
-print(f"The answer to the question: {models[best_model].__class__.__name__} - {best_score:.3f}")
+scores = {model.__class__.__name__ : fit_predict_eval(model, X_train_norm, X_test_norm, y_train, y_test) for model in models.values()}
+
+print("The answer to the 1st question: yes")
+
+print("The answer to the 2nd question:", ", ".join([model[0] + "-" + str(round(model[1], 3)) for model in sorted(scores.items(), key=lambda x: x[1], reverse=True)][:2]))
